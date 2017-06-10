71892a0259bbc1f5f7c4d304ef53484112869ab6
Mark Walker <markwalker@Marks-MacBook-Pro.local>
2017-06-10 17:09:02 -0400

    updating TVC tabs

--- Resibeam/Beams/tvc_BeamDesign.swift
+++ Resibeam/Beams/tvc_BeamDesign.swift
@@ -22,7 +22,7 @@ class tvc_BeamDesign: NSTabViewController {
         
         theTabView.font = tabFont!
         
-        self.tabViewItems.removeLast(2)
+        self.tabViewItems.removeLast(1)
 
         
     }
